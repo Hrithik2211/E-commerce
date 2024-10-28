247c5ccddc905d2a506bf71f3608acdce3f41e80
Hrithik Yadav <hrithik22lko@gmail.com>
2024-10-28 22:00:07 +0530

    fixed products page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
-import { AnimatePresence, motion } from 'framer-motion';
 import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Shop from './Pages/Shop';
 import Product from './Pages/Product';
 import LoginSignup from './Pages/LoginSignup';
@@ -12,142 +11,33 @@ import men_banner from './Components/Assets/banner_mens.png';
 import women_banner from './Components/Assets/banner_women.png';
 import kid_banner from './Components/Assets/banner_kids.png';
 
-const pageVariants = {
-  initial: {
-    opacity: 0,
-    x: '-100vw',
-  },
-  in: {
-    opacity: 1,
-    x: 0,
-  },
-  out: {
-    opacity: 0,
-    x: '100vw',
-  },
-};
-
-const pageTransition = {
-  type: 'tween',
-  ease: 'anticipate',
-  duration: 0.5,
-};
-
-function AnimatedRoutes() {
-  const location = useLocation();
-
-  return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route
-          path="/"
-          element={
-            <motion.div
-              initial="initial"
-              animate="in"
-              exit="out"
-              variants={pageVariants}
-              transition={pageTransition}
-            >
-              <Shop />
-            </motion.div>
-          }
-        />
-        <Route
-          path="/men"
-          element={
-            <motion.div
-              initial="initial"
-              animate="in"
-              exit="out"
-              variants={pageVariants}
-              transition={pageTransition}
-            >
-              <ShopCategory banner={men_banner} category="men" />
-            </motion.div>
-          }
-        />
-        <Route
-          path="/women"
-          element={
-            <motion.div
-              initial="initial"
-              animate="in"
-              exit="out"
-              variants={pageVariants}
-              transition={pageTransition}
-            >
-              <ShopCategory banner={women_banner} category="women" />
-            </motion.div>
-          }
-        />
-        <Route
-          path="/kids"
-          element={
-            <motion.div
-              initial="initial"
-              animate="in"
-              exit="out"
-              variants={pageVariants}
-              transition={pageTransition}
-            >
-              <ShopCategory banner={kid_banner} category="kid" />
-            </motion.div>
-          }
-        />
-        <Route
-          path="/product"
-          element={
-            <motion.div
-              initial="initial"
-              animate="in"
-              exit="out"
-              variants={pageVariants}
-              transition={pageTransition}
-            >
-              <Product />
-            </motion.div>
-          }
-        />
-        <Route
-          path="/cart"
-          element={
-            <motion.div
-              initial="initial"
-              animate="in"
-              exit="out"
-              variants={pageVariants}
-              transition={pageTransition}
-            >
-              <Cart />
-            </motion.div>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <motion.div
-              initial="initial"
-              animate="in"
-              exit="out"
-              variants={pageVariants}
-              transition={pageTransition}
-            >
-              <LoginSignup />
-            </motion.div>
-          }
-        />
-      </Routes>
-    </AnimatePresence>
-  );
-}
-
 function App() {
   return (
     <div>
+      {/* setting up react router for faster load speed */}
       <BrowserRouter>
         <Navbar />
-        <AnimatedRoutes />
+        <Routes>
+          {/* to navigate through pages faster */}
+          <Route path="/" element={<Shop />} />
+          <Route
+            path="/men"
+            element={<ShopCategory banner={men_banner} category="men" />}
+          />
+          <Route
+            path="/women"
+            element={<ShopCategory banner={women_banner} category="women" />}
+          />
+          <Route
+            path="/kids"
+            element={<ShopCategory banner={kid_banner} category="kid" />}
+          />
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
+        </Routes>
         <Footer />
       </BrowserRouter>
     </div>
